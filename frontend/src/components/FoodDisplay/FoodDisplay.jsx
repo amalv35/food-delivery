49a6056f817d2236840504ec68e5f891a2a5ceb5
amalv35 <amalvijayakumar035@gmail.com>
2025-12-29 23:36:22 +0530

    feat: :sparkles:  #1 implement FoodDisplay & FoodItem component

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
+
 
 const FoodDisplay = () => {
    
-  const {food_list} = useContext(StoreContext)
+ const { food_list } = useContext(StoreContext);
 
   return (
-    <div className='food-diaplay' id='food-dsplay'>
-      <h2>top dishes near you</h2>
+    <div className='food-display' id='food-display'>
+      <h2>Top dishes near you</h2>
       <div className='food-display-list'>
-        {food_list.map((item,index)=>{
-        return
+        {food_list.map((item, index) => {
+          return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
         })}
-    </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./foodItem.css";
+import { assets } from "../../assets/assets";
 
-const FoodItem = () => {
+const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
+ 
   return (
-    <div>FoodItem</div>
-  )
-}
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt={name} />
+        {!itemCount ? (
+          <img 
+            className="add-food-item" 
+            onClick={() => setItemCount(prev => prev + 1)} 
+            src={assets.add_icon_white} 
+            alt="Add to cart"
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img 
+              onClick={() => setItemCount(prev => prev - 1)} 
+              src={assets.remove_icon_red} 
+              alt="Remove" 
+            />
+            <p>{itemCount}</p>
+            <img 
+              onClick={() => setItemCount(prev => prev + 1)} 
+              src={assets.add_icon_green} 
+              alt="Add more" 
+            />
+          </div> 
+        )}
+      </div>
+  
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="ratings" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
+};
 
-export default FoodItem
+export default FoodItem;
