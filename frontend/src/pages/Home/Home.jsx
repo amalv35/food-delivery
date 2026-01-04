49ab57aa1fb8b9abe7c23f5a063e8b7a2bb601a7
amalv35 <amalvijayakumar035@gmail.com>
2026-01-04 14:22:22 +0530

    feat: :sparkles: #5implement appdownload

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import IntroAnimation from '../../components/IntroComponent/IntroComponent'
+import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
      const [category, setCategory] = useState("All")
   return (
@@ -13,6 +14,7 @@ const Home = () => {
         <Header />
          <ExploreMenu category={category} setCategory={setCategory} />
          <FoodDisplay category={category} />
+         <AppDownload />
     </div>
     </>
   )
