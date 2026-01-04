40fca63558fbe0d06710398bcc22939e832d2a44
amalv35 <amalvijayakumar035@gmail.com>
2026-01-04 13:49:31 +0530

    feat: :sparkles: #4 improve visibilty of card text

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,14 +3,18 @@ import "./home.css"
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import IntroAnimation from '../../components/IntroComponent/IntroComponent'
 const Home = () => {
      const [category, setCategory] = useState("All")
   return (
+    <>
+    <IntroAnimation />
     <div>
         <Header />
          <ExploreMenu category={category} setCategory={setCategory} />
          <FoodDisplay category={category} />
     </div>
+    </>
   )
 }
 
